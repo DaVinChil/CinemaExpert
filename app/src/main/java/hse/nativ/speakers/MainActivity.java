793646a3a4269c1863f56846961a82acd88bcfd3
IlyaSekunov <sekunovila@gmail.com>
2023-03-23 18:39:05 +0300

    ad

--- app/src/main/java/hse/nativ/speakers/MainActivity.java
+++ app/src/main/java/hse/nativ/speakers/MainActivity.java
@@ -2,6 +2,7 @@ package hse.nativ.speakers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = new Intent(MainActivity.this, second.class);
+        startActivity(intent);
         int a = 10;
         int mod = a % 3;// loh
         System.out.println("Hello, world!");
