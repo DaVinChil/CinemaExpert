b52f923bca0db5829ba807db9be02f07067fdd07
Neto test <fox.winner9@gmail.com>
2023-03-31 14:36:34 +0300

    Change intro page

--- app/src/main/java/hse/nativ/speakers/SignLogActivity.java
+++ app/src/main/java/hse/nativ/speakers/SignLogActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextPaint;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
@@ -19,8 +20,7 @@ public class SignLogActivity extends AppCompatActivity {
 
     private TextView cinemaTtl;
     private TextView expertTtl;
-    private MaterialButton loginBtn;
-    private MaterialButton signupBtn;
+    private MaterialButton getStartedBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +28,14 @@ public class SignLogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_log);
 
         findAll();
-        setAppearingAnim();
         setGradientOnTitle();
 
         setButtons();
     }
 
     protected void setButtons(){
-        loginBtn.setOnClickListener((v) -> {
+        getStartedBtn.setOnClickListener((v) -> {
             Intent intent = new Intent(SignLogActivity.this, LogInActivity.class);
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-            startActivity(intent);
-        });
-        signupBtn.setOnClickListener((v) -> {
-            Intent intent = new Intent(SignLogActivity.this, SignUpActivity.class);
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             startActivity(intent);
         });
@@ -50,20 +44,7 @@ public class SignLogActivity extends AppCompatActivity {
     protected void findAll(){
         cinemaTtl = findViewById(R.id.cinema_text);
         expertTtl = findViewById(R.id.expert_text);
-        loginBtn = findViewById(R.id.log_in_button);
-        signupBtn = findViewById(R.id.main_sign_up_button);
-    }
-
-    protected void hideViews(View... views){
-        for(View v : views){
-            v.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    protected void revealViews(View... views){
-        for(View v : views){
-            v.setVisibility(View.VISIBLE);
-        }
+        getStartedBtn = findViewById(R.id.get_started_button);
     }
 
     protected void setGradientOnTitle(){
@@ -77,39 +58,5 @@ public class SignLogActivity extends AppCompatActivity {
                 }, null, Shader.TileMode.CLAMP);
         expertTtl.getPaint().setShader(textShader);
         expertTtl.setShadowLayer(1, 0, 0, Color.BLACK);
-    }
-
-    protected void setAppearingAnim(){
-        hideViews(cinemaTtl, expertTtl, loginBtn, signupBtn);
-
-        viewTranslationY(cinemaTtl, 210f, 1);
-        viewTranslationY(expertTtl, 210f, 1);
-
-        revealViews(cinemaTtl, expertTtl);
-
-        new Handler().postDelayed(() -> {
-            viewTranslationY(cinemaTtl, 10f, 700);
-            viewTranslationY(expertTtl, 10f, 700);
-        }, 1000);
-
-        new Handler().postDelayed(()->{
-            revealWithAnim(500, loginBtn, signupBtn);
-        }, 1700);
-    }
-
-    protected void revealWithAnim(int duration, View... views){
-        for(View v : views){
-            v.setAlpha(0f);
-            revealViews(v);
-            v.animate().alpha(1f)
-                    .setDuration(duration)
-                    .setListener(null);
-        }
-    }
-
-    protected void viewTranslationY(View v, float offset, int duration){
-        ObjectAnimator anim = ObjectAnimator.ofFloat(v, "translationY", offset);
-        anim.setDuration(duration);
-        anim.start();
     }
 }
